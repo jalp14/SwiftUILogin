4f80a7b0b15faed8dab23fc1137739b9ae20f7bc
Jalp <jalpdesai@icloud.com>
2019-07-01 12:50:46 +0100

    Initial Commit

---
+++ SwiftUILogin/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUILogin
+//
+//  Created by Jalp on 01/07/2019.
+//  Copyright © 2019 JDC0rp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
